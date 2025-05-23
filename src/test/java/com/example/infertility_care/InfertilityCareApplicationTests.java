46f85ab73f1930484c7ab7b6bff6041ba5194bdb
NguyenNguDang <dq0202345@gmail.com>
2025-05-23 17:07:15 +0700

    test

---
+++ src/test/java/com/example/infertility_care/InfertilityCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infertility_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfertilityCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
